66c533afbd05f47c807658b1664ad1b5b4bafad5
hugo <hugo.goisot@lyceeastier.com>
2024-03-17 13:31:24 +0100

    ajout des packages CRC16 et Clavier

---
+++ src/main/java/fr/btsciel/td_modbus_javafx/crc16/CRC16.java
@@ -0,0 +1,58 @@
+package fr.btsciel.td_modbus_javafx.crc16;
+
+import fr.btsciel.td_modbus_javafx.clavier.In;
+
+public class CRC16 {
+    public static int stdPoly = 0xA001; // polynome arbitraire du crc16
+    public static int intialValue = 0xffff;
+
+    public static void main(String[] args) {
+        String saisie;
+        String regex = "([0-9]|[a-zA-Z])([0-9]|[a-zA-Z])";
+
+//        mettre un if avec la verification du regex
+        System.out.print("veuillez écrire votre trame d'octets: ");
+        saisie = In.readString();
+
+        System.out.println("le CRC= " + calculCrc16(formatage(saisie), intialValue, stdPoly));
+
+    }
+    public static int calculCrc16(byte[] octets, int valeurInitiale, int polynomeArbitraire){
+        int crc = valeurInitiale;
+        int nb_octet_traite = 0;
+        byte octetSuivant = octets[0];
+        do {
+            int octet_traite = octetSuivant;
+            crc = crc ^ octet_traite;
+            for (int i = 0; i < 8; i++) {
+                if(crc % 2 == 1){
+                    crc = (crc/2)^polynomeArbitraire;
+                }else{
+                    crc /= 2;
+                }
+                nb_octet_traite++;
+            }
+        }while(nb_octet_traite <= octets.length);
+        return crc;
+    }
+    public static byte[] hexStringByteEnArray(String message){
+
+        int len = message.length();
+        byte[] data = new byte[len/2];
+        for (int i = 0; i < len; i+=2) {
+            data[i/2] = (byte) ((Character.digit(message.charAt(i),16) << 4) +
+                    (Character.digit(message.charAt(i+1), 16)));
+        }
+        return data;
+    }
+    public static byte[] formatage(String trame){
+        trame = trame.trim().replaceAll(" ", "");
+        if(trame.length() % 2 == 1){
+            return hexStringByteEnArray('0'+trame);
+        }else{
+            return hexStringByteEnArray(trame);
+        }
+    }
+
+
+}
